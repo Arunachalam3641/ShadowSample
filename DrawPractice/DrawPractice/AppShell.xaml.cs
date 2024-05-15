ac318448e28b5dffe2dedcd0d3e301b95a577e11
Arunachalam3641 <arunachalam.sf3641@gmail.com>
2024-05-15 16:06:38 +0530

    Added the sample

---
+++ DrawPractice/DrawPractice/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DrawPractice
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DrawPractice/DrawPractice/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+namespace DrawPractice
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var height = 42;
+            var width = 38;
+            //float arrowHeight = 30;
+            //float arrowWidth = arrowHeight / 2;
+            //float centerPosition = height / 2;
+            //float leftPosition = (width / 2) - (arrowWidth / 2);
+            //float topPosition = (height / 2) - (arrowHeight / 2);
+            //float rightPosition = (width / 2) + (arrowWidth / 2);
+            //float bottomPosition = (height / 2) + (arrowHeight / 2);
+            //PathF path = new PathF();
+            //path.MoveTo(leftPosition, topPosition);
+            //path.LineTo(rightPosition, centerPosition);
+            //path.LineTo(leftPosition, bottomPosition);
+            //Canvas.DrawPath(path);
+        }
+
+        
+    }
+
+}
